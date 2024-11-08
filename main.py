98c26a0a0fb5ae651ec82a4226e5d56672ef1410
Yogesh Ladhe <yogesh@sportzinteractive.com>
2024-11-08 20:19:55 +0530

    code change

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 from pyspark.sql import SparkSession
-
+from pyspark.sql import functions as f
 def main():
     spark = SparkSession.builder \
         .appName("PySpark Docker Example") \
         .getOrCreate()
 
-    data = [("Alice", 1), ("Bob", 2), ("Charlie", 3)]
-    columns = ["Name", "Value"]
+    data = [("Alice", 1,100), ("Bob", 2,300), ("Charlie", 3,400)]
+    columns = ["Name", "id","sal"]
     df = spark.createDataFrame(data, columns)
     print(spark.version)
+
+    df = df.withColumn("sal_with_bonus",f.col("sal")+f.lit(20))
     df.show()
 
     spark.stop()
