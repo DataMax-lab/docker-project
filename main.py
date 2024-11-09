37cd6512bf109dd64fa10fd0c2f11084e3efd9f3
Yogesh Ladhe <yogesh@sportzinteractive.com>
2024-11-09 08:49:58 +0530

    emp transformation

--- main.py
+++ main.py
@@ -3,11 +3,29 @@ def process_emp_data():
     spark = SparkSession.builder \
         .appName("PySpark Docker Example") \
         .getOrCreate()
+    print(spark.version)
 
     data = [("Alice", 1,100), ("Bob", 2,300), ("Charlie", 3,400)]
     columns = ["Name", "id","sal"]
+
+
     df = spark.createDataFrame(data, columns)
-    print(spark.version)
+    df.createOrReplaceTempView("vw_emp")
+
+    sql= f"""
+                with tab_sql as
+                (
+                    select  row_number()over(order by sal )rno ,
+                            id,
+                            name,
+                            sal
+                    from    vw_emp
+                )
+                select  *
+                from    tab_sql
+        """
+
+    df = spark.sql(sql)
 
     df.show()
 
